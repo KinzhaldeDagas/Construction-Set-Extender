ee2f0bfa15a3b99a296700da177111cdceb8639f
KinzhaldeDagas <classicmello@icloud.com>
2026-02-24 19:57:38 -0500

    Fix release linker settings for common and CrashRpt

--- StdAfx.cpp
+++ StdAfx.cpp
@@ -12,7 +12,9 @@
 #pragma comment(lib, "UxTheme.lib")
 #pragma comment(lib, "mscoree.lib")
 #pragma comment(lib, "Psapi.lib")
+#if defined(NDEBUG) && !defined(WAIT_FOR_DEBUGGER)
 #pragma comment(lib, "CrashRpt1403.lib")
+#endif
 
 #pragma comment(linker, "\"/manifestdependency:type='win32' \
 name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
