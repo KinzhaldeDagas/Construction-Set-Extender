cf5240d60786b1e7a7a2e8706ee41334cc098250
KinzhaldeDagas <classicmello@icloud.com>
2026-02-26 11:34:37 -0500

    Fix RC shim parsing and CrashRpt library name

--- afxres.h
+++ afxres.h
@@ -1,11 +1,17 @@
 #pragma once
 
 // Compatibility shim for environments where MFC is not installed.
+// For resource compiler invocations, include the canonical Win32 header directly
+// because RC preprocessor support is limited compared to the C/C++ compiler.
+#ifdef RC_INVOKED
+#include <winres.h>
+#else
 // Prefer the canonical Win32 resource header and fall back to winresrc.h.
-#if __has_include(<winres.h>)
+#if defined(__has_include) && __has_include(<winres.h>)
 #include <winres.h>
 #else
 #include <winresrc.h>
+#endif
 #endif
 
 // MFC's afxres.h also provides IDC_STATIC used by many .rc dialogs.
